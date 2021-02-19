4c4d800a86c143a4bbe10ce99d061eb3f61dc8b8
Allex372 <62441924+Allex372@users.noreply.github.com>
2021-02-19 14:46:33 +0200

    init

--- app.js
+++ app.js
@@ -1,33 +1,89 @@
 const path = require('path');
 const fs = require('fs');
-const dirName = __dirname + '/1800';
-const dirNamefemale = __dirname + '/2000'
-const filePath = __dirname + '/1800/Alina.txt'
-const filePath2 = __dirname+ '/1800/Allex.txt'
+const dirName = path.join(__dirname, '1800');
+const dirNamefemale = path.join(__dirname + '/2000');
+const filePath = path.join(__dirname + '/1800/Alina.txt');
+const filePath2 = path.join(__dirname+ '/1800/Allex.txt');
+const filePathStar = path.join(__dirname + '/StarTask')
 
-// fs.readFile(path.join(filePath), (err, data) => {
-//     console.log(JSON.parse(data).gender)
+//Task 1 Star
+// function upFile(filePathStar1){
+//     fs.readdir(filePathStar1,((err, files) => {
+//         if (err){
+//             console.log(err)
+//             return;
+//         }
+//         for (const file of files) {
+//             fs.stat(path.join(filePathStar1,file),(err1, stats) => {
+//                 console.log(stats.isDirectory())
+//                 if (stats.isDirectory()){
+//                     upFile(path.join(filePathStar1,file))
+//                 }else {
+//                     fs.rename(path.join(filePathStar1, file), path.join(filePathStar,file), err2 => {
+//                         if (err2){
+//                             console.log(err2)
+//                         }
+//                     })
+//                 }
+//                 if (err1){
+//                     console.log(err1)
+//                 }
+//             })
+//         }
+//     }))
+// }
+// upFile(filePathStar)
+
+function upFile(filePathDinamic){
+    fs.readdir(filePathDinamic, (err, files) => {
+        if (err){
+            console.log(err)
+            return;
+        }
+        for (const file of files) {
+            fs.stat(path.join(filePathDinamic, file), (err1, stats) => {
+                if (stats.isDirectory()){
+                    upFile(path.join(filePathDinamic, file))
+                }else {
+                    fs.rename(path.join(filePathDinamic,file), path.join(filePathStar,file), err2 => {
+                        if (err2){
+                            console.log(err2)
+                        }
+                    })
+                }
+                if (err1){
+                    console.log(err1)
+                }
+            })
+        }
+    })
+
+}
+upFile(filePathStar)
+
+
+//Task1 MALE
+// fs.readdir(dirName, (err,files)=>{
+//     if (err){
+//         console.log(err)
+//         return;
+//     }
+//     for (const file of files) {
+//         fs.readFile(path.join(dirName,file), (err1, data) => {
+//             console.log(JSON.parse(data).gender);
+//             if (JSON.parse(data).gender === 'male'){
+//                 fs.rename(path.join(dirName,file), path.join(__dirname,'2000',file), err2 => {
+//                     if (err2){
+//                         console.log(err2)
+//                     }
+//                 })
+//             }
+//         })
+//     }
+//     console.log(files)
 // })
-fs.readdir(dirName, (err,files)=>{
-    if (err){
-        console.log(err)
-        return;
-    }
-    for (const file of files) {
-        fs.readFile(path.join(dirName,file), (err1, data) => {
-            console.log(JSON.parse(data).gender);
-            if (JSON.parse(data).gender === 'male'){
-                fs.rename(path.join(dirName,file), path.join(__dirname,'2000',file), err2 => {
-                    if (err2){
-                        console.log(err2)
-                    }
-                })
-            }
-        })
-    }
-    console.log(files)
-})
 
+//Task1 Female
 // fs.readdir(dirNamefemale, (err,files)=>{
 //     if (err){
 //         console.log(err)
